d961c5f84f72c1a94c9187b7f56aa26a5689f466
herenet <herenet@126.com>
2025-05-29 19:06:10 +0800

    配置验证回调也移到api系统中

--- app/SaaSAdmin/Controllers/Manager/OrderConfigController.php
+++ app/SaaSAdmin/Controllers/Manager/OrderConfigController.php
@@ -471,7 +471,7 @@ class OrderConfigController extends Controller
                     'bundle_id' => $bundle_id,
                     'message' => '回调验证中，请稍后...',
                 ];
-                Cache::connection('api_cache')->put($cache_key, $call_back_verify_status, self::APPLE_CALLBACK_VERIFY_CACHE_TTL);
+                Cache::connection('api_cache')->set($cache_key, $call_back_verify_status, self::APPLE_CALLBACK_VERIFY_CACHE_TTL);
                 sleep(2);
                 $testNotificationStatus = $api->getTestNotificationStatus($response);
 
